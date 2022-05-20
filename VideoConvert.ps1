aa283ea733f2571723fb882c8f7f82cca42a2c78
yaplatonov <ya.platonov@gmail.com>
2022-05-20 12:05:00 +0300

    Add readme. Some changes scripts.

--- VideoConvert.ps1
+++ VideoConvert.ps1
@@ -1,25 +1,28 @@
 $path = "F:\Архив\Новости\Новости 2018\Январь\10\"
 $ffmpeg_exe = "F:\ffmpeg\ffmpeg.exe"
 $ffmpeg_params = " -vf bwdif -c:v libx264 -preset fast -crf 17 -c:a aac -b:a 160k -r 25 "
+$types = @("*.avi","*.m2p")
 
-# Get recursive files from folder in $path
-Get-ChildItem -Path $path -Recurse -Force -Include *.avi - |
+$log_file = $path + "logfile.txt"
+If ((Test-Path $log_file) -eq $False) { New-Item -Path $log_file -ItemType "file" -Force }
+
+### Get recursive files from folder in $path
+Get-ChildItem -Path $path -Recurse -Force -Include $types |
      ForEach-Object {
-       $log_file = $_.DirectoryName + "\logfile.txt"
-       #Write-Output "Files are successfully created in $env:computername" >> $log_file
-       $out_file = $_.DirectoryName + "\" + $_.BaseName + ".mp4"
-       #Write-Output $log_file
-       #Write-Output $out
-        ### Обычный путь без пробелов и киррилицы
-        #$ffmpeg_cmd = $ffmpeg_exe + " -i " + $_.FullName + $ffmpeg_params + $out_file
-       ### Экранирование кавычек для путей с пробелами
+
+       ### Обычный путь без пробелов и киррилицы
+       #$ffmpeg_cmd = $ffmpeg_exe + " -i " + $_.FullName + $ffmpeg_params + $out_file
+       ### Добавление кавычек для путей с пробелами
        $ffmpeg_cmd = $ffmpeg_exe + " -i " + '"' + $_.FullName + '"'  + $ffmpeg_params + '"' + $out_file + '"'
        #If outfile is already exists, delete
-       If ((Test-Path $out_file) -eq $True) {
-        Remove-Item $out_file -Force
-        }
-       #Write-Output $ffmpeg_cmd
+       If ((Test-Path $out_file) -eq $True) { Remove-Item $out_file -Force }
+       
+       ### Run
        cmd /c $ffmpeg_cmd
-       #Write-Output $_.FullName
+       
+
+    ### Write some logs
+    $log = (Get-Date -Format "MM/dd/yyyy HH:mm") + " " + $_.FullName + " converted."
+    Write-Output $log >> $log_file
        Remove-Item $_.FullName -Force -Verbose
      }
---
+++ VideoConvert_LowRes.ps1
@@ -0,0 +1,30 @@
+﻿$path = "D:\_Downloads\_del\"
+$ffmpeg_exe = "D:\ffmpeg\ffmpeg.exe"
+$ffmpeg_params = " -c:v -vf scale=-2:480 libx264 -preset ultrafast -crf 28 -c:a aac -b:a 128k  "
+
+$types = @("*.avi","*.mkv","*.mp4")
+
+
+$log_file = $path + "logfile.txt"
+If ((Test-Path $log_file) -eq $False) { New-Item -Path $log_file -ItemType "file" -Force }
+
+#Get files in folder path
+Get-ChildItem -Path $path -Recurse -Force -Include $types |
+  ForEach-Object {
+
+    $out_file = $_.DirectoryName + "\общая\" + $_.BaseName + ".mp4"
+       
+    ### Обычный путь без пробелов и киррилицы
+    #$ffmpeg_cmd = $ffmpeg_exe + " -i " + $_.FullName + $ffmpeg_params + $out_file
+    ### Экранирование кавычек для путей с пробелами
+    $ffmpeg_cmd = $ffmpeg_exe + " -i " + '"' + $_.FullName + '"'  + $ffmpeg_params + '"' + $out_file + '"'
+    ## If outfile is already exists, delete
+    If ((Test-Path $out_file) -eq $True) { Remove-Item $out_file -Force }
+    #Write-Output $ffmpeg_cmd
+    cmd /c $ffmpeg_cmd
+    
+    #Write some logs
+    $log = (Get-Date -Format "MM/dd/yyyy HH:mm") + " " + $_.BaseName + " converted."
+    Write-Output $log >> $log_file
+    Remove-Item $_.FullName -Force -Verbose
+  }
